db520734ce63ce9d27dc59740e811ec95b36750f
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2015-12-15 15:32:42 -0800

    Remove print for development

--- page_viewer.php
+++ page_viewer.php
@@ -11,15 +11,15 @@
   	$page_viewer=true;
 		$user_namel = explode('-',$_GET['user_name']); 
 		$_SESSION['url'] = $user_namel;
-		print_r($user_namel );
+		// print_r($user_namel );
 		// print_r($url); 
 		// set up this part if the algorithm of the url changes
 		//$_SESSION['table_id']=$url[0];//the first value in the url
 		$data['type']=$url[1]; // type=article,look in the second link in the sub url 
 		// helper::load_page_profile_detail_lookdetails($url); 
-		// $_SESSION['url']=$url;  
+		// $_SESSION['url']=$url;    
 	echo "</div>";
-
+	
 	if ($data['type']=='article'){  
 		// require('detail.php');
 	}
