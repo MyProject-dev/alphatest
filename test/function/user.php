c5060c58d46d31c9ad192d550eeee5c13adc78f3
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2015-12-16 23:58:58 -0800

    add overall reset functionality

--- test/function/user.php
+++ test/function/user.php
@@ -86,3 +86,58 @@ echo "my total follower " . $user->getTotalFollower() . "\n<br>";
 echo "my total following " . $user->getTotalFollowing() . "\n<br>"; 
 
 echo "my total favorited " . $user->getTotalFavorited() . "\n<br>";
+
+
+
+
+
+
+
+
+
+/*
+ *
+ * Delete data from fs
+ */
+
+$tableNameArray = array(
+    'activity'=>'_table_id',
+    'fs_brands'=>'bno',
+    'fs_cflag'=>'flagno',
+    'fs_comment'=>'cno',
+    'fs_drip_modals'=>'dmno',
+    'fs_favorite_modals'=>'fmno',
+    'fs_flag'=>'flno',
+    'fs_follow'=>'flno',
+    'fs_info_advertise'=> 'advertise_id',
+    'fs_keyword'=>'kno',
+    'fs_look_flag'=>'flr_no', 
+    'postedlooks'=>'plno',
+    'posted_looks_comments'=>'plcno',
+    'posted_looks_comments_like_dislike'=>'plcldno',
+    'fs_postedarticles'=>'artno',
+    'fs_post_attribute'=>'id',
+    'fs_pltag'=>'pltgno',
+    'fs_notification'=>'nno',
+    'fs_member_accounts'=>'mano',
+    'fs_members'=>'mno',
+    'fs_member_timeline'=>'mptno',
+    'fs_message'=>'msgno',
+    'fs_search'=>'sno'
+);
+
+
+
+foreach($tableNameArray as $key=>$value) {
+
+    if(mysql_query("delete from $key where $value > 0")){
+        echo "$key deleted where $key > 0 \n <br>";
+    } else {
+        echo "failed to delete table $key where $value > 0 \n <br>";
+    }
+}
+ 
+
+
+
+
