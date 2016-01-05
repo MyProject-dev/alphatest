572f4237aef3a65f921569a6f88a5736f3576c5f
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-01-05 18:43:01 +0800

    Topic drop down in read article connected to topic database table.
    So - when select category - topic of the category should show.

--- fs_folders/modals/postarticle/topics.php
+++ fs_folders/modals/postarticle/topics.php
@@ -1,43 +1,55 @@
 <?php
+    require("../../../fs_folders/php_functions/connect.php");
     require("../../../fs_folders/php_functions/function.php");
     require("../../../fs_folders/php_functions/Database/Database.php");
     require("../../../fs_folders/php_functions/Class/Article.php");
     require("../../../fs_folders/php_functions/Class/Reset.php");
     require("../../../fs_folders/php_functions/Class/Topic.php");
-    require("../../../fs_folders/php_functions/Class/Category.php"); 
+    require("../../../fs_folders/php_functions/Class/Category.php");
+    require("../../../fs_folders/php_functions/Class/TopicCategory.php");
 
     use App\Topic;
+    use App\TopicCategory;
     use App\Category;
+
     $db = new Database();
     $db->connect();
     $mno = 1021;
     $topic = new Topic( $db, $mno);
-    $category1 = new Category($db, $mno); 
-    
-    /**
-    * just add space after '<?php echo $val; ?> ', like that instead of  '<?php echo $val; ?>' to add new line of topics. 
-    */ 
+    $category1 = new Category($db, $mno);
+    $topicCategory = new TopicCategory($db, $mno);
 
+    //    echo "testing <br>";
+
+    /**
+    * just add space after '<?php echo $val; ?> ', like that instead of  '<?php echo $val; ?>' to add new line of topics.
+    */
     $category = (!empty($_GET['category']))? $_GET['category'] : '';
     $page = (!empty($_GET['page']))? $_GET['page'] : '';
     $search = (!empty($_GET['search']))? $_GET['search'] : '';
-    // echo "search for  $search  <br>"; 
-    // echo  ' category = ' . $category . '<br><br>'; 
+    // echo "search for  $search  <br>";
+    // echo  ' category = ' . $category . '<br><br>';
     $response = TRUE;
 
-    if($category == 'Beauty') {
-        $topics = array('Beauty Book Review', 'Beauty Event', 'Beauty Workshop', 'Beauty Recipe', 'Celebrity Hair', 'Celebrity Makeup', 'Celebrity Nails', 'Celebrity Skin Care', 'Eye of the day', 'Face of The Day', 'Hair Tutorial', 'Hair Review', 'Nails Tutorial', 'Lip of the day', 'Makeup Haul', 'Makeup Review', 'Makeup Sale', 'Makeup Tool Review', 'Makeup Tutorial', 'Product Comparison ', 'Skincare Review', 'Hair', 'Nails', 'Make up', 'Skincare', 'Fragrance', 'Celebrity & Influencer  ', 'Fitness ', 'Diet & Nutrition', 'Cosmetic Treatment', 'Product Review', 'Product Haul', 'Make Sale', 'Beauty Event', 'Shopping Tips'); 
-    } elseif($category == 'Entertainment') {
-        $topics = array( 'Art ', 'Book', 'Celebrity Gossip', 'Concert', 'Festival', 'Movie ', 'Music', 'Television Show'); 
-    } elseif($category == 'Fashion') {
-        $topics = array('Celebrity ', 'Designer', 'DIY Fashion', 'Fashion Event', 'Fashion News', 'Fashion Photography', 'Mens Fashion', 'Product Review', 'OOTD ', 'Shopping', 'Store', 'Street Style', 'Styling Tip', 'Trends');
-    } elseif($category == 'Lifestyle') {
-        $topics = array('Automobile', 'Business', 'Cooking ', 'DIY & Crafts', 'Entertaining', 'Family & Friends', 'Food & Drinks', 'Health', 'Home', 'Money', 'Travel', 'Work'); 
-    } else {
-        $topics = array('');
+    /**
+     * This function covers only for read article drop down
+     * Get id of the category selected
+     * @return $id
+     */
+    $id = $topicCategory->getIdByName($category);
+
+    /**
+     * Get all the topic by the category id
+     * @return array()
+     */
+    $topicsArray = $topic->getTopicsByCategoryId($id);
+
+    /**
+     * Flatten array
+     */
+    for ($i = 0; $i < count($topicsArray); $i++) {
+        $topics[] = $topicsArray[$i]['name'];
     }
-
-
 ?>
     <?php if($page == 'rate-article') { ?>
         <option>Select Topic</option>
@@ -64,7 +76,9 @@
                 <?php endif;?>
             </tbody>
         </table> 
-    <?php } else { ?> 
+    <?php } else { ?>
+
+
         <table border="0" cellpadding="0" cellspacing="0" style="margin-left: -16px;width: 280px !important;" >
             <tbody>
             <?php for ($i=0; $i < count($topics); $i++) {  ?>
