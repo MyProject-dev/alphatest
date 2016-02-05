49d5094698e30c3e8bb5736ef96c6217f1fdc219
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-02-05 23:01:36 +0800

    admin fix logout function

--- fs_folders/admin/php_file/logout.php
+++ fs_folders/admin/php_file/logout.php
@@ -1,6 +1,9 @@
 <?php 
-	session_start();
-	require ('../../php_functions/myclass.php');
+	session_start(); 
+
+    require("../../../fs_folders/php_functions/connect.php");
+	require("../../../fs_folders/php_functions/function.php");
+	require("../../../fs_folders/php_functions/myclass.php"); 
 	$mc = new myclass(); 
 	unset($_SESSION['adm_no']);
 
